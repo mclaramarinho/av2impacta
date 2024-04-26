0af57a335bb6ff367b225f36623040d12c863555
Clara Marinho <c.marinho.barreto@avanade.com>
2024-04-26 15:42:21 -0300

    MVC

---
+++ Escola/Controllers/CursosController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Escola.Controllers
+{
+    public class CursosController : Controller
+    {
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
